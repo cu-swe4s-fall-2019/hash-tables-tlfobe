9f602da726cd6c18bd418d57f2f7d6e98c6f9b10
tlfobe <lennyfobe@gmail.com>
2019-10-23 19:23:32 -0600

    added h_ascii function+tests, started documentation for h_rolling, added .travis.yml

--- hash_functions.py
+++ hash_functions.py
@@ -1,6 +1,50 @@
 
 def h_ascii(key, N):
-    return None
+    """
+    generates a hash value for a given key using a linear hash fxn
+
+    Arguments
+    ---------
+    key : string
+        key used to get hash value
+    N : int
+        size of hash table
+
+    Returns
+    -------
+    hash index : int
+        location in hashtable to place value
+    """
+    if not isinstance(key, str):
+        raise TypeError("h_ascii: key "+str(key)+" is not a string!")
+    if not isinstance(N, int):
+        raise TypeError("h_ascii: N "+str(N)+" is not an int!")
+
+    s = 0
+
+    for i in range(len(key)):
+        s += ord(key[i])
+    return s % N
+
 
 def h_rolling(key, N):
+    """
+    generates a hash value for a given key using a polynomial hash fxn
+
+    Arguments
+    ---------
+    key : string
+        key used to get hash value
+    N : int
+        size of hash table
+
+    Returns
+    -------
+    hash index : int
+        location in hashtable to place value
+    """
+    if not isinstance(key, str):
+        raise TypeError("h_rolling: key "+str(key)+" is not a string!")
+    if not isinstance(N, int):
+        raise TypeError("h_rolling: N "+str(N)+" is not an int!")
     return None
---
+++ test_hash_function.py
@@ -0,0 +1,31 @@
+import unittest
+import hash_functions
+import random
+
+
+class TestHashFunctions(unittest.TestCase):
+    def test_h_ascii_incorrect_inputs(self):
+        self.assertRaises(TypeError, hash_functions.h_ascii,
+                          ["list", "of", "strings"], 10)
+        self.assertRaises(TypeError, hash_functions.h_ascii, "key", "string!")
+
+    def test_h_ascii_simple_ascii(self):
+        assert(hash_functions.h_ascii("A", 130) == 65)
+
+    def test_h_ascii_wrap_number(self):
+        assert(hash_functions.h_ascii("AAAA", 10) == 65*4 % 10)
+
+    def test_h_ascii_random(self):
+        key = ''
+        count = 0
+        for _ in range(random.randint(1, 10)):
+            val = random.randint(97, 122)
+            key += chr(val)
+            count += val
+        rand_N = random.randint(1, 1000)
+        assert hash_functions.h_ascii(key, rand_N) == count % rand_N
+
+    def test_h_rolling(self):
+        self.assertRaises(TypeError, hash_functions.h_ascii,
+                          ["list", "of", "strings"], 10)
+        self.assertRaises(TypeError, hash_functions.h_ascii, "key", "string!")
