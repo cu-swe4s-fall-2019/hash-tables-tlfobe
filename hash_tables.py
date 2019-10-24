98dbb10c04ded94c6fe510ef18eaccf81b6e3993
tlfobe <lennyfobe@gmail.com>
2019-10-23 23:14:23 -0600

    added LinearProbe hash method +testing

--- hash_tables.py
+++ hash_tables.py
@@ -1,29 +1,118 @@
 import hash_functions
+import sys
 
 
 class LinearProbe:
-    def __init__(self, N, hash_fucntion):
-        self.hash_fucntion = hash_fucntion
-        self.N = N
+    """
+    Class used for creating a linear probing hash table
+
+    Attributes
+    ----------
+    hash_function : function
+        specific hash_function from hash_functions module
+    N : int
+        size of hash table
+    table : list
+        initialized hash table where keys + value pairs will populate
+    capacity : int
+        number of entries in the hash table
+    
+    Methods
+    -------
+    __init__(hash_function, N)
+        constructor method for LinearProbe
+    add(key, value)
+        add key+value pair into hash table
+    search(key)
+        search for a value given a key
+    """
+    def __init__(self, hash_function, N = None):
+        """
+        constructor method for Linear Probe
+
+        Arguments
+        ---------
+        hash_function : function
+            specific hash_function from hash_functions module
+        N : int
+            size of hash table
+        """
+        if N is None:
+            self.N = sys.maxsize
+        elif not isinstance(N, int):
+            raise TypeError("LinearProbe: N "+str(N)+" is not an int!")
+        else:
+            self.N = N
+
+        if hash_function.__name__ not in ["h_FNV", "h_ascii", "h_rolling"]:
+            raise NotImplementedError("LinearProbe: hash function " + hash_function.__name__ + " is not implemented!")
+        self.hash_function = hash_function
+        self.table = [ None for i in range(N)]
+        self.capacity = 0
 
     def add(self, key, value):
-        start_hash = self.hash_fucntion(key, self.N)
-        pass
+        """
+        adds a value+key pair to the has table
+        
+        Arguments
+        ---------
+        key : string
+            string key used to creat hash
+        value : anything!
+            value to be stored hash value of key
+
+        Returns
+        -------
+        True : bool
+            returns True if value+key pair was added
+        """
+        start_hash = self.hash_function(key, self.N)
+        
+        for i in range(self.N):
+            hash_position = (start_hash + i) % self.N
+            if self.table[hash_position] is None:
+                self.table[hash_position] = (key, value)
+                self.capacity += 1
+                return True
+        raise IndexError("LinearProbe.search: hashtable is full!")
 
     def search(self, key):
-        start_hash = self.hash_fucntion(key, self.N)
-        pass
+        """
+        search a key and return a value in the hashtable
+
+        Arguments
+        ---------
+        key : string
+            string key to access hash table
+        
+        Returns
+        -------
+        value : anything!
+            value coresponding to hash adress
+        """
+        start_hash = self.hash_function(key, self.N)
+        
+        for i in range(self.N):
+            hash_position = (start_hash + i) % self.N
+            if self.table[hash_position] is None:
+                    raise IndexError("LinearProbe.search: key " + key +
+                                     " is not in hashtable")
+            if self.table[hash_position][0] == key:
+                return self.table[hash_position][1]
+        raise IndexError(("LinearProbe.search: key " + key +
+                                     " is not in hashtable"))
 
 
 class ChainedHash:
-    def __init__(self, N, hash_fucntion):
+    def __init__(self, N, hash_function):
         self.hash_fucntion = hash_fucntion
         self.N = N
+        pass
 
     def add(self, key, value):
-        start_hash = self.hash_fucntion(key, self.N)
+        start_hash = self.hash_function(key, self.N)
         pass
 
     def search(self, key):
-        start_hash = self.hash_fucntion(key, self.N)
+        start_hash = self.hash_function(key, self.N)
         pass
---
+++ test_hash_tables.py
@@ -0,0 +1,45 @@
+import unittest
+import random
+import sys
+import hash_tables
+import hash_functions
+
+class TestLinearProbe(unittest.TestCase):
+    def test_linear_probe_incorrect_types(self):
+        self.assertRaises(TypeError, hash_tables.LinearProbe, hash_functions.h_ascii, "not an int!")
+        self.assertRaises(NotImplementedError, hash_tables.LinearProbe, lambda a,b: None, 50)
+        
+    def test_linear_probe_add_empty(self):
+        table = hash_tables.LinearProbe(hash_functions.h_ascii, 100)
+        assert(table.add('woah!', 1) == True)
+
+    def test_linear_probe_search_1(self):
+        table = hash_tables.LinearProbe(hash_functions.h_ascii, 100)
+        table.add('woah!', 1)
+        assert(table.search('woah!') == 1)
+
+    def test_linear_probe_add_search_random(self):
+        table = hash_tables.LinearProbe(hash_functions.h_ascii, 100)
+        key = ''
+        for _ in range(random.randint(1, 10)):
+            val = random.randint(97, 122)
+            key += chr(val)
+        value = random.randint(0,10000)
+        assert(table.add(key, value) == True)
+        assert(table.search(key) == value)
+
+    def test_linear_probe_full_random(self):
+        size = random.randint(1, 10000)
+        table = hash_tables.LinearProbe(hash_functions.h_ascii, size)
+        for i in range(size):
+            table.add(str(i), i)
+        
+        self.assertRaises(IndexError, table.add, 'full', 10)
+        self.assertRaises(IndexError, table.search, 'full')
+
+    def test_linear_probe_key_not_in_table(self):
+        table = hash_tables.LinearProbe(hash_functions.h_ascii, 30)
+        self.assertRaises(IndexError, table.search, 'not in table')
+
+class TestChainedHash:
+    pass
