07be3ea0b12ff36e79555fbdbe835a8276ee604a
tlfobe <lennyfobe@gmail.com>
2019-10-24 13:48:54 -0600

    working plot output

--- collision_resolution.py
+++ collision_resolution.py
@@ -1,6 +1,8 @@
 import hash_functions
 import hash_tables
 import matplotlib
+import time
+import sys
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import argparse
@@ -37,14 +39,19 @@ def main():
     args = parser.parse_args()
 
     # Read in file to array of keys
-
-    with open(args.filename, 'r') as f_handle:
-        keys = []
-        for l in f_handle.readlines():
-            keys.append(l.rstrip())
+    try:
+        with open(args.filename, 'r') as f_handle:
+            keys = []
+            for l in f_handle.readlines():
+                keys.append(l.rstrip())
+    except IsADirectoryError:
+        print("collision_resolution: file selected was not a datafile", file=sys.stderr)
+        sys.exit(1)
+    except FileNotFoundError:
+        print("collision_resolution: please input a valid file path", file=sys.stderr)
+        sys.exit(1)
 
     hash_fxn = eval("hash_functions.h_"+args.hash_fxn)
-    col_res = eval("hash_tables."+args.col_res)
 
     # Build subplot based on size of col_resols and hash_fxns
     hash_vals = []
@@ -60,7 +67,51 @@ def main():
 
     plt.savefig(args.out_id+"_"+args.hash_fxn+".png", bbox_inches='tight')  
 
+    # Build time to load/search plot
 
+    col_res = eval("hash_tables."+args.col_res)
+
+
+    N = len(keys)
+    if args.col_res == "QuadraticProbe":
+        N = int(2*N+1) # Quadratic Probing is not guarenteed to fill the entire array
+
+    hash_table = col_res(hash_fxn, N = N)
+    times_to_add = []
+    load_factors = []
+    for word in keys:
+        t0 = time.time()
+        hash_table.add(word, word)
+        t1 = time.time()
+        times_to_add.append(t1 - t0)
+        load_factors.append(hash_table.capacity/hash_table.N)
+
+    fig = plt.figure(figsize=(3,3),dpi=300)    
+    ax = fig.add_subplot(1,1,1)
+
+    ax.plot(load_factors, times_to_add, '.', ms=1, alpha=0.5)
+    ax.set_xlabel("Load Factor")
+    ax.set_ylabel("Add Time")
+    
+    plt.savefig(args.out_id+"_"+args.col_res+"_"+args.hash_fxn+"_add.png", bbox_inches='tight')  
+
+    # Build time to load/search plot
+
+    times_to_search = []
+    for word in keys:
+        t0 = time.time()
+        hash_table.search(word)
+        t1 = time.time()
+        times_to_search.append(t1 - t0)
+
+    fig = plt.figure(figsize=(3,3),dpi=300)    
+    ax = fig.add_subplot(1,1,1)
+
+    ax.plot(list(range(1, len(times_to_search)+1)), times_to_search, '.', ms=1, alpha=0.5)
+    ax.set_xlabel("Hashed Word")
+    ax.set_ylabel("Search Time")
+    
+    plt.savefig(args.out_id+"_"+args.col_res+"_"+args.hash_fxn+"_search.png", bbox_inches='tight')  
 
 
 
