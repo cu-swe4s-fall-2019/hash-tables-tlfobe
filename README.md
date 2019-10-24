bf3ee0648c52d866355b1aa6d86875ec9de5efd3
tlfobe <lennyfobe@gmail.com>
2019-10-24 01:11:49 -0600

    started benchmarking file, collison_resolution file and added badge to rEADME

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hash tables
+
+[![Build Status](https://travis-ci.com/cu-swe4s-fall-2019/hash-tables-tlfobe.svg?branch=master)](https://travis-ci.com/cu-swe4s-fall-2019/hash-tables-tlfobe)
---
+++ collision_resolution.py
@@ -0,0 +1,20 @@
+import hash_functions
+import hash_tables
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import argparse
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
