54a38d2172f50160a537da532cb29ec0cc1748b0
tlfobe <lennyfobe@gmail.com>
2019-10-23 21:08:40 -0600

    added FNV hashfunction +tests

--- hash_functions.py
+++ hash_functions.py
@@ -1,3 +1,5 @@
+import sys
+
 
 def h_ascii(key, N):
     """
@@ -54,3 +56,33 @@ def h_rolling(key, N):
     s = s % 2**64
 
     return s % N
+
+
+def h_FNV(key, N = None):
+    """
+    generates a hash for a given key using the FNV hash fxn
+
+    Arguments
+    ---------
+    key : string
+        key used to get hash value
+    N (optional) : int
+        size of hashtable, if unspecified returns for hashtable size of 2^64
+    """
+    if not isinstance(key, str):
+        raise TypeError("h_rolling: key "+str(key)+" is not a string!")
+    if N == None:
+        pass
+    elif not isinstance(N, int):
+        raise TypeError("h_rolling: N "+str(N)+" is not an int!")
+
+    hash_v = 14695981039346656037 # FNV offset basis
+    for char in key:
+        hash_v = hash_v * 1099511628211 & sys.maxsize # FNV prime
+        hash_v = hash_v ^ ord(char) & sys.maxsize
+    if N == None:
+        return hash_v & sys.maxsize
+    else:
+        return hash_v % N # FNV hash functions ideal don't need wrap, because
+                        # modulo is slow!
+        
